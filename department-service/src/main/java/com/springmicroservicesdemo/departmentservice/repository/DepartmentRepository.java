7d869bbbf097baadbbd53a3db0ab74c2ed1f9586
bhanutejabt <bhanutejabt@gmail.com>
2022-04-20 12:49:44 +0530

    refactor

---
+++ department-service/src/main/java/com/springmicroservicesdemo/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.springmicroservicesdemo.departmentservice.repository;
+
+import com.springmicroservicesdemo.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentId(Long departmentId);
+}
